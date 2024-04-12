77c4c5334da8e40f2bf357e101cc24b8f847d71b
Lambari <78892876+AndreiBoeck@users.noreply.github.com>
2024-04-12 08:26:50 -0300

    Rotacionar e Envelhecer ja aplicados, main atualizada

--- imagem.c
+++ imagem.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+struct Pixel
 {
-    int R;
-    int G;
-    int B;
-} RGB;
+    int R, G, B;
+};
+
 
 typedef struct{
     int coluna;
@@ -15,43 +14,133 @@ typedef struct{
 } TAMANHO;
 
 int inverte(int pixel){
+    // Subtrair valor RGB da variavel valor no struct TAMANHO
     int pixel_invertido;
-
 
 
     return pixel_invertido;
 }
 
+struct Pixel **gira(struct Pixel **matrix, int linhas, int colunas) {
+    struct Pixel **matriz = (struct Pixel **)malloc(colunas * sizeof(struct Pixel *));
+    for (int i = 0; i < colunas; i++)
+    {
+        matriz[i] = (struct Pixel *)malloc(linhas * sizeof(struct Pixel));
+    }
+
+    for (int i = 0; i < linhas; ++i) {
+        for (int j = 0; j < colunas; ++j) {
+            matriz[j][i].R = matrix[i][j].R;
+            matriz[j][i].G = matrix[i][j].G;
+            matriz[j][i].B = matrix[i][j].B;
+        }
+    }
+    return matriz;
+}
+
+struct Pixel **envelhecer(struct Pixel **matrix, int linhas, int colunas) {
+    for (int i = 0; i < linhas; ++i) {
+        for (int j = 0; j < colunas; ++j) {
+            matrix[i][j].R -= 30;
+            matrix[i][j].G -= 15;
+            matrix[i][j].B -= 15;
+        }
+    }
+    return matrix;
+}
 
 int main(void){
     FILE *fp;
-    fp = fopen("UwU.ppm","r");
-
-    char formato[3];
-    fscanf(fp, "%s", formato); 	// lê o tipo de imagem P3 (color), P2 (P&B)
-    printf("%s\n", formato);
-
-    TAMANHO tamanho;
-    tamanho.coluna = 0;
-    tamanho.linha = 0;
-    fscanf(fp, "%d %d", &tamanho.coluna, &tamanho.linha); 	// lê o tamanho da matriz
-    printf("%d %d\n", tamanho.coluna, tamanho.linha);
-
-    fscanf(fp, "%d", &tamanho.valor); 	// lê o valor máximo.
-    printf("%d\n", tamanho.valor);
-
-    int pixel[tamanho.coluna][tamanho.linha];
-
-    int i, j;
-    RGB rgb;
-    for(j=0; j<tamanho.linha; j++)
-    {
-        for(i=0; i<tamanho.coluna; i++)
-        {
-            fscanf(fp, "%d %d %d", &rgb.R, &rgb.G,&rgb.B);
-            printf("%d %d %d\n", rgb.R, rgb.G,rgb.B);
+    int c;
+    char tipoImg[3];
+    int i, j, linhas, colunas, valor, r, g, b;
+    int escolha = -1;
+    char img[50];
+    wprintf(L"Qual arquivo deseja usar?\n");
+        scanf("%s", img);
+    while(escolha != 0) {
+        printf("%s\n", img);
+        fp = fopen(img, "r");
+        if(fp == NULL){
+            fprintf(stderr, "Erro ao abrir o arquivo %s.\n", img);
+            exit(EXIT_FAILURE);
         }
+
+        // Lê o cabeçalho do arquivo
+        fscanf(fp, "%s", tipoImg);                    // lê o tipo de imagem P3
+        fscanf(fp, "%d %d", &colunas, &linhas);    // lê o tamanho da matriz
+        fscanf(fp, "%d", &valor);                   // lê o valor máximo.
+
+        // Alocação da matriz de pixels RGB
+        struct Pixel **matriz = (struct Pixel **) malloc(linhas * sizeof(struct Pixel *));
+        for (i = 0; i < linhas; i++) {
+            matriz[i] = (struct Pixel *) malloc(colunas * sizeof(struct Pixel));
+        }
+        // Leitura dos valores de pixel RGB
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas; j++) {
+                fscanf(fp, "%d %d %d", &r, &g, &b);
+                matriz[i][j].R = r;
+                matriz[i][j].G = g;
+                matriz[i][j].B = b;
+            }
+        }
+
+        // Fecha o arquivo.
+        fclose(fp);
+
+        wprintf(L"                      Escolha o que deseja:\n"
+                        "[1] - Gerar imagens tons de cinza      [2] - Gerar imagem negativa\n"
+                        "[3] - Aumentar brilho                  [4] - Diminuir brilho\n"
+                        "[5] - Girar 90 graus                   [6] - Envelhecer a imagem\n"
+                        "                           [0] - Sair\n");
+        scanf("%d", &escolha);
+
+
+        struct Pixel **matriz2;
+        switch (escolha) {
+            case 1: {
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+                matriz2 = gira(matriz, linhas, colunas);
+                break;
+            }
+            case 6: {
+                matriz2 = envelhecer(matriz, linhas, colunas);
+                break;
+            }
+            default:
+                fprintf(stderr, "Opção invalida.\n");
+                exit(EXIT_FAILURE);
+        }
+
+        FILE *fp_novo = fopen("ImgAlterada.ppm", "w");
+        if (fp_novo == NULL) {
+            fprintf(stderr, "Erro ao criar o arquivo 'ImgAlterada.ppm'.\n");
+            exit(EXIT_FAILURE);
+        }
+        // Gravação dos valores de pixel RGB
+        fprintf(fp_novo, "P3\n%d %d\n255\n", colunas, linhas);
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas; j++) {
+                fprintf(fp_novo, "%d %d %d ", matriz2[i][j].R, matriz2[i][j].G, matriz2[i][j].B);
+            }
+            fprintf(fp_novo, "\n"); // Adiciona uma nova linha após cada linha de pixels.
+        }
+        fclose(fp);
     }
 
-
+    // Fecha o arquivo.
+    printf("-------------------------------FIM-----------------------------\n");
 }
+
