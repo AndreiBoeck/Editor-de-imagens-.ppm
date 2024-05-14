067071d48d708eba3e4d0195517425e017cac237
Andrei <andreicunhaboeck@gmail.com>
2024-05-14 02:06:54 -0300

    Comments

---
+++ cmake-build-debug/CMakeFiles/Trabalho.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabalho.dir/imagem.c.obj"
+  "CMakeFiles/Trabalho.dir/imagem.c.obj.d"
+  "Trabalho.exe"
+  "Trabalho.exe.manifest"
+  "Trabalho.pdb"
+  "libTrabalho.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trabalho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
